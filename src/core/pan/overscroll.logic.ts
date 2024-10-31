00899fc8c573a4a6bf864ac9fa30a2617c03a10e
ruben.kuckelkorn <ruben.kuckelkorn@check24.de>
2024-10-31 07:42:55 +0100

    temp code comments

--- src/core/pan/overscroll.logic.ts
+++ src/core/pan/overscroll.logic.ts
@@ -3,7 +3,6 @@ import { getContext } from "../../utils";
 import {
   isOverscrollBehaviourAllowedYAxis,
   isOverscrollBehaviourAllowed,
-  isOverscrollBehaviourAllowedXAxis, calculateDirection
 } from "./panning.utils";
 
 // determine if preventDefault and stopPropagation should be called
@@ -20,42 +19,42 @@ export function handleOverscrollBehaviour(contextInstance: ReactZoomPanPinchCont
     return;
   }
 
-  const { positionY, positionX, previousPositionX, previousPositionY } = getContext(contextInstance).state;
+  const { positionY/*, positionX, previousPositionX, previousPositionY*/ } = getContext(contextInstance).state;
 
   // TODO: Do I need to use min/max-X/Y bounds here? The values seem to be wrong?
   const clientHeight = contextInstance.wrapperComponent.clientHeight;
-  const clientWidth = contextInstance.wrapperComponent.clientWidth;
+  // const clientWidth = contextInstance.wrapperComponent.clientWidth;
 
   const scrollHeight = contextInstance.wrapperComponent.scrollHeight;
-  const scrollWidth = contextInstance.wrapperComponent.scrollWidth;
+  // const scrollWidth = contextInstance.wrapperComponent.scrollWidth;
 
-  const { x: startX, y: startY } = contextInstance.startCoords;
+  const { /*x: startX, */y: startY } = contextInstance.startCoords;
 
-  const touchDeltaX = event.touches[0].clientX - positionX - startX;
+  // const touchDeltaX = event.touches[0].clientX - positionX - startX;
   const touchDeltaY = event.touches[0].clientY - positionY - startY;
 
-  const componentDirection = calculateDirection({ x: positionX, y: positionY, prevX: previousPositionX, prevY: previousPositionY });
+  // const componentDirection = calculateDirection({ x: positionX, y: positionY, prevX: previousPositionX, prevY: previousPositionY });
 
   // TODO: startCoords are touch x position - positionX which comes from transformState. Can I calculate that backwards?
   //  const { positionX, positionY } = contextInstance.transformState;
   //  const x = touches[0].clientX;
   //  const y = touches[0].clientY;
   //  contextInstance.startCoords = { x: x - positionX, y: y - positionY };
-  const touchDirection = calculateDirection({ x: event.touches[0].clientX, y: event.touches[0].clientY, prevX: (positionX + startX), prevY: (positionY + startY) });
+  // const touchDirection = calculateDirection({ x: event.touches[0].clientX, y: event.touches[0].clientY, prevX: (positionX + startX), prevY: (positionY + startY) });
 
   // console.log({ componentDirection, touchDirection });
 
   const minY = 0;
   const maxY = scrollHeight - clientHeight;
 
-  const minX = 0;
-  const maxX = scrollWidth - clientWidth;
+  // const minX = 0;
+  // const maxX = scrollWidth - clientWidth;
 
   const yRounded = Math.round(positionY * -1);
-  const xRounded = Math.round(positionX * -1);
+  // const xRounded = Math.round(positionX * -1);
 
   const isOverscrollYAxisAllowed = isOverscrollBehaviourAllowedYAxis(contextInstance);
-  const isOverscrollXAxisAllowed = isOverscrollBehaviourAllowedXAxis(contextInstance);
+  // const isOverscrollXAxisAllowed = isOverscrollBehaviourAllowedXAxis(contextInstance);
 
   // console.log({ Y: isOverscrollYAxisAllowed, X: isOverscrollXAxisAllowed });
 
